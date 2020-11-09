ff6b6c946b38c369ba76d6cd64ea0d17ae927f92
manishgautam30 <manish.gautam_cs18@gla.ac.in>
2020-11-10 01:41:50 +0530

    implemented methods like getcount,onCret,onbind view Holder

--- app/src/main/java/com/example/grossifyapp/adpter/RecentlyViewedAdapter.java
+++ app/src/main/java/com/example/grossifyapp/adpter/RecentlyViewedAdapter.java
@@ -2,6 +2,7 @@ package com.example.grossifyapp.adpter;
 
 import android.content.Context;
 import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -21,8 +22,24 @@ public class RecentlyViewedAdapter extends RecyclerView.Adapter<RecentlyViewedAd
         this.recentlyViewedList = recentlyViewedList;
     }
 
-   public static class RecentlyViewedHolder extends RecyclerView.ViewHolder{
-        public RecentlyViewedHolder(@NonNull View itemView){
+    @NonNull
+    @Override
+    public RecentlyViewedViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+
+    public void onBindViewHolder(@NonNull RecentlyViewedViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return recentlyViewedList.size();
+    }
+
+    public static class RecentlyViewedViewHolder extends RecyclerView.ViewHolder{
+        public RecentlyViewedViewHolder(@NonNull View itemView){
             super(itemView);
         }
    }
