a6cb0368717b4f6092c8aa604c7f7f54c1c9ec6b
manishgautam30 <manish.gautam_cs18@gla.ac.in>
2020-11-10 01:19:01 +0530

    constructor defined

---
+++ app/src/main/java/com/example/grossifyapp/adpter/RecentlyViewedAdapter.java
@@ -0,0 +1,21 @@
+package com.example.grossifyapp.adpter;
+
+import android.content.Context;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.grossifyapp.model.RecentlyViewed;
+
+import java.util.List;
+
+public class RecentlyViewedAdapter extends RecyclerView.Adapter<RecentlyViewedAdapter.RecentlyViewedViewHolder> {
+
+    Context context;
+    List<RecentlyViewed> recentlyViewedList;
+
+    public RecentlyViewedAdapter(Context context, List<RecentlyViewed> recentlyViewedList) {
+        this.context = context;
+        this.recentlyViewedList = recentlyViewedList;
+    }
+
+}
