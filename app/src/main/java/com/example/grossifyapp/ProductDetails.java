c902fb0da7abe3fb9e03ca41cb2a00451286266c
manishgautam30 <manish.gautam_cs18@gla.ac.in>
2020-11-10 03:15:45 +0530

    variables defined

--- app/src/main/java/com/example/grossifyapp/ProductDetails.java
+++ app/src/main/java/com/example/grossifyapp/ProductDetails.java
@@ -3,8 +3,18 @@ package com.example.grossifyapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ProductDetails extends AppCompatActivity {
+
+
+    ImageView img, back;
+    TextView proName, proPrice, proDesc, proQty, proUnit;
+
+    String name, price, desc, qty, unit;
+    int image;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
