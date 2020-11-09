a8785192ed2fe21ff965882b1f1b742b0a9a62ee
manishgautam30 <manish.gautam_cs18@gla.ac.in>
2020-11-10 01:15:33 +0530

    getter setter for RecentlyViewed java class

--- app/src/main/java/com/example/grossifyapp/model/RecentlyViewed.java
+++ app/src/main/java/com/example/grossifyapp/model/RecentlyViewed.java
@@ -18,4 +18,53 @@ public class RecentlyViewed {
         this.imageUrl = imageUrl;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public int getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(int imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+
 }
