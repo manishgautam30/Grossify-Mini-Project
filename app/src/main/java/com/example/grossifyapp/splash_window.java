fcc70dca8808ecaf772cb847b6f750aa7bbb09fc
manishgautam30 <manish.gautam_cs18@gla.ac.in>
2020-11-10 14:26:43 +0530

    changes added in splash Activity

--- app/src/main/java/com/example/grossifyapp/splash_window.java
+++ app/src/main/java/com/example/grossifyapp/splash_window.java
@@ -3,22 +3,51 @@ package com.example.grossifyapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-
-import java.util.*;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class splash_window extends Activity {
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.splashwindow);
-            Timer time = new Timer();
-            time.schedule(new TimerTask() {
-                @Override
-                public void run() {
-                    Intent in = new Intent(splash_window.this, MainActivity.class);
-                    startActivity(in);
-                    finish();
-                }
-            }, 2000);       // Splash window's time is taken of 2 seconds
-        }
+
+    private static int SPLASH_SCREEN = 5000;
+
+    //Variables
+    Animation logo_anim, bottom_anim;
+    ImageView image;
+    TextView logo;
+    TextView slogan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.splashwindow);
+
+        //Animation
+        logo_anim = AnimationUtils.loadAnimation(this,R.anim.logo_animation);
+        bottom_anim = AnimationUtils.loadAnimation(this, R.anim.logo_bottom_anim);
+
+
+        //Hooks
+        image = findViewById(R.id.imageView);
+        logo = findViewById(R.id.grossify);
+        slogan = findViewById(R.id.textView3);
+
+        image.setAnimation(logo_anim);
+        logo.setAnimation(bottom_anim);
+        slogan.setAnimation(bottom_anim);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(splash_window.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
+    }
 }
